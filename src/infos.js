faa538a35b5584d223b7c75d6e0dfe9e312afdc8
Adrien EXCOFFIER <adrien.excoffier@epsi.fr>
2018-05-17 11:41:18 +0200

    thoune

--- src/infos.js
+++ src/infos.js
@@ -1,5 +1,26 @@
 var client = require('./esConnexion.js');
 
-client.cluster.health({},function(err,resp,status) {  
-  console.log("-- Client Health --",resp);
-});
+
+export function search(index,type, title){
+  return client.search({  
+    index: index,
+    type: type,
+    body: {
+      query: {
+        match: { title:title }
+      },
+    }
+  },function (error, response,status) {
+      if (error){
+        console.log("search error: "+error)
+      }
+      else {
+        console.log("--- Response ---");
+        console.log(response);
+        console.log("--- Hits ---");
+        response.hits.hits.forEach(function(hit){
+          console.log(hit);
+        })
+      }
+  });
+}
--- src/logic.js
+++ src/logic.js
@@ -1,10 +1,13 @@
 import { cdi } from './cdi'
+import { search } from './infos'
 
 export const logic = ($message) => {
+    const offer = "bot-offer"
+    console.log($message)
     return new Promise((resolve, reject) => {
-        const response = cdi[Math.floor(Math.random() * cdi.length)];
-    const newMessage = Object.assign($message.message, { content: response })
-
+    const response = cdi[Math.floor(Math.random() * cdi.length)];
+    const newMessage = Object.assign($message, { content: response })
+    console.log(newMessage, search(offer,"offer",$message.intent.intent))
     resolve(newMessage)
     })
 }
