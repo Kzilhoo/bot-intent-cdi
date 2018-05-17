1b0c704097b43738c08d80b859840444ae3d4223
Adrien EXCOFFIER <adrien.excoffier@epsi.fr>
2018-05-17 22:01:24 +0200

    Url solved

--- src/logic.js
+++ src/logic.js
@@ -3,14 +3,13 @@ import { search } from './infos'
 
 export const logic = ($message) => {
     const offer = "bot-offer"
-    const domain = "informatique"
     return new Promise((resolve, reject) => {
     const response = cdi[Math.floor(Math.random() * cdi.length)];
     const intent = $message.intent.intent
     const findedEntity = $message.entities.find((item)=>item.type=="domaine")
 
-    const query = search(offer,"offer",intent,findedEntity.entity).then((answerFindedEntity)=>{
-        const findUrl = findedEntity[0]._source.url
+    const query = search(offer,"offer",intent,findedEntity.entity).then((dbEntity)=>{
+        const findUrl = dbEntity[0]._source.url
         const newMessage = Object.assign($message, { content: response + findUrl})
         resolve(newMessage)
         })
