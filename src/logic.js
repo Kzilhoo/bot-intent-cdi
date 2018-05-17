92d381ff88fe59e1e53d7d97e29bc2ca2dee1836
Adrien EXCOFFIER <adrien.excoffier@epsi.fr>
2018-05-17 23:40:29 +0200

    Changes to the announce

--- src/logic.js
+++ src/logic.js
@@ -4,15 +4,17 @@ import { search } from './infos'
 export const logic = ($message) => {
     const offer = "bot-offer"
     return new Promise((resolve, reject) => {
-        const response = cdi[Math.floor(Math.random() * cdi.length)];
+
         const intent = $message.intent.intent
         const findedEntity = $message.entities.find((item) => item.type == "domaine")
 
         const query = search(offer, "offer", intent, findedEntity.entity).then((dbEntity) => {
+            const response = cdi[Math.floor(Math.random() * cdi.length)];
+            const firstAnswer = Object.assign({}, $message, { content: response })
             const answerArray = dbEntity.map((dbMap) => {
-                return Object.assign({}, $message, { content: response + dbMap._source.url })
+                return Object.assign({}, $message, { content: dbMap._source.url })
             })
-            resolve(answerArray)
+            resolve([].concat([firstAnswer], answerArray, ))
         })
     })
 }
